9c81323577faa3e62a126eabc9bf4997bf843de3
yama-chiiii <nagayama0409@gmail.com>
2024-12-02 20:04:20 +0900

    feat: tailwindCSSの設定

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,14 @@
 import type { Config } from "tailwindcss";
 
+const range = (start: number, end: number): number[] =>
+  Array.from({ length: end - start + 1 }, (_, i) => start + i);
+
+const generateSpacing = (): Record<string, string> =>
+  range(1, 800).reduce((acc, i) => {
+    acc[i] = `${i}px`;
+    return acc;
+  }, {} as Record<string, string>);
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,8 +18,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        brown: {
+          dark: "#4C1015",
+          DEFAULT: "#D0AD74",
+          light: "#F9F2DF",
+        },
+        red: {
+          dark: "#861D47",
+        },
+      },
+      spacing: {
+        px: "1px",
+        ...generateSpacing(),
+      },
+      fontFamily: {
+        mPlus: ['"M PLUS Rounded 1c"', 'serif'],
       },
     },
   },
