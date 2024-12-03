cdeb03640dd230fd31f01f686d7e64bf40149f5f
渡邉　空登 <noah@Noahs-MacBook-Air.local>
2024-12-03 16:22:17 +0900

    fix: ヘッダーの微調整

--- src/app/component/Header.tsx
+++ src/app/component/Header.tsx
@@ -2,33 +2,41 @@ export default function Header(){
     return(
         <div className="z-50">
             <div className="fixed w-full flex top-0">
-                <button type="button" className="top-0 bg-brown-dark flex text-white font-mPlus px-20 py-25 font-semibold text-xl">
+                <button type="button" className="top-0 bg-brown-dark flex text-white font-mPlus px-20 py-20 font-semibold text-xl">
                     リノマチ！
                 </button>
-                <div className="flex ml-auto mr-5">
+                <div className="flex items-center ml-auto mr-5">
                     <div className="ml-10 pt-5">
-                        <button type="button" className="w-36 h-36">
+                        <button type="button" className="pl-3 w-38 h-35">
                             <img src="/watch.svg" alt="Icon"/>
                         </button>
-                        <div className="font-mPlus font-semibold text-xs">最近見た</div>
+                        <div className="font-mPlus font-semibold text-xxs">
+                            最近見た
+                        </div>
                     </div>
                     <div className="ml-10 pt-5">
-                        <button type="button" className="pl-13 w-36 h-36">
+                        <button type="button" className="pl-13 w-48 h-35">
                             <img src="/description.svg" alt="Icon"/>
                         </button>
-                        <div className="font-mPlus font-semibold text-xs">保存した条件</div>
+                        <div className="font-mPlus font-semibold text-xxs">
+                            保存した条件
+                        </div>
                     </div>
                     <div className="ml-10 pt-5">
-                        <button type="button" className="pl-6 w-36 h-36">
+                        <button type="button" className="pl-7 w-42 h-35">
                             <img src="/favorite.svg" alt="Icon"/>
                         </button>
-                        <div className="font-mPlus font-semibold text-xs">おきにいり</div>
+                        <div className="font-mPlus font-semibold text-xxs">
+                            おきにいり
+                        </div>
                     </div>
                     <div className="ml-10 pt-5">
-                        <button type="button" className="w-36 h-36">
+                        <button type="button" className="pl-3 w-38 h-35">
                             <img src="/person.svg" alt="Icon"/>
                         </button>
-                        <div className="font-mPlus font-semibold text-xs">ログイン</div>
+                        <div className="font-mPlus font-semibold text-xxs">
+                            ログイン
+                        </div>
                     </div>
                 </div>
             </div>
