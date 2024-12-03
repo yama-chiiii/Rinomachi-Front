b9ff1f4e137031a34e7c504b738a5387282e1ea3
yama-chiiii <nagayama0409@gmail.com>
2024-12-03 12:00:44 +0900

    feat: ファイル作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="w-full min-h-screen flex flex-col items-center bg-brown-light font-mPlus font-semibold text-2xl">
-      <div className="text-brown-dark">home</div>
+    <div className="w-full min-h-screen flex flex-col items-center font-mPlus font-semibold text-2xl">
+      <div className="w-1/2 h-screen bg-brown-light text-red-dark">home</div>
     </div>
   );
 }
---
+++ src/app/search/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <div className="w-full min-h-screen flex flex-col items-center font-mPlus font-semibold text-2xl">
+        <div className="w-1/2 bg-brown-light text-red-dark">home</div>
+      </div>
+    );
+  }
