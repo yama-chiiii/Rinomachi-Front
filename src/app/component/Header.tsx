91279ea43d3b08430f9c1f7f5b0ef79f74f85e54
渡邉　空登 <noah@Noahs-MacBook-Air.local>
2024-12-03 20:46:04 +0900

    fix: ヘッダーのボタンのパディングの調整

--- src/app/component/Header.tsx
+++ src/app/component/Header.tsx
@@ -2,11 +2,11 @@ export default function Header(){
     return(
         <div className="z-50">
             <div className="fixed w-full flex top-0">
-                <button type="button" className="top-0 bg-brown-dark flex text-white font-mPlus px-20 py-20 font-semibold text-xl">
-                    リノマチ！
+                <button type="button" className="top-0 bg-brown-dark flex text-white font-mPlus px-20 py-20 font-bold text-xl">
+                    リノマチ!
                 </button>
                 <div className="flex items-center ml-auto mr-5">
-                    <div className="ml-10 pt-5">
+                    <div className="ml-10 pt-14">
                         <button type="button" className="pl-3 w-38 h-35">
                             <img src="/watch.svg" alt="Icon"/>
                         </button>
@@ -14,7 +14,7 @@ export default function Header(){
                             最近見た
                         </div>
                     </div>
-                    <div className="ml-10 pt-5">
+                    <div className="ml-10 pt-14">
                         <button type="button" className="pl-13 w-48 h-35">
                             <img src="/description.svg" alt="Icon"/>
                         </button>
@@ -22,7 +22,7 @@ export default function Header(){
                             保存した条件
                         </div>
                     </div>
-                    <div className="ml-10 pt-5">
+                    <div className="ml-10 pt-14">
                         <button type="button" className="pl-7 w-42 h-35">
                             <img src="/favorite.svg" alt="Icon"/>
                         </button>
@@ -30,7 +30,7 @@ export default function Header(){
                             おきにいり
                         </div>
                     </div>
-                    <div className="ml-10 pt-5">
+                    <div className="ml-10 pt-14">
                         <button type="button" className="pl-3 w-38 h-35">
                             <img src="/person.svg" alt="Icon"/>
                         </button>
