ecd3c03b3f88ad95641b6b1226532c3b68dd9178
渡邉　空登 <noah@Noahs-MacBook-Air.local>
2024-12-06 11:33:43 +0900

    feat: 検索ページの要素の作成（未完成）

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,7 +5,11 @@ export default function Search() {
       <>
         <Header></Header>
         <div className="w-full min-h-screen flex flex-col items-center font-mPlus font-semibold text-2xl">
-          <div className="w-full h-screen bg-brown-light text-red-dark">home</div>
+          <div className="w-full h-screen bg-brown-light text-red-dark">
+            <div className="bg-white mt-140 mx-30">
+              <div className="font-mPlus text-brown-dark">エリアから探す</div>
+            </div>
+          </div>
         </div>
       </>
       
