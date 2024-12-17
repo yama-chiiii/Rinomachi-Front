42a9c2988a9d228b5181f6576689c05abd4d98cf
渡邉　空登 <noah@Noahs-MacBook-Air.local>
2024-12-18 01:03:56 +0900

    fix :npm run devのエラーの解消

--- src/app/component/Line.tsx
+++ src/app/component/Line.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Link from "next/link";
+// import Link from "next/link";
 import {useState} from 'react';
 
 function CheckBox({name,id,setIsChecked}:{name:string,id:string,setIsChecked:() => void}){
