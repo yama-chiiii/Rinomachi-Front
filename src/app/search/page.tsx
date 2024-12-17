884ddcfa9017d5a5eb6ccb325e2901df15332b22
yama-chiiii <nagayama0409@gmail.com>
2024-12-17 23:13:40 +0900

    fix: ESlintエラー

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,6 @@
 // import Link from "next/link";
 // import Header from "./../component/Header";
+import Image from "next/image";
 
 export default function Search() {
     return (
@@ -8,7 +9,7 @@ export default function Search() {
           <div className="w-full h-full bg-brown-light text-red-dark">
             <div className="bg-white rounded mt-120 mb-20 mx-30">
               <div className="flex border-b border-dashed border-brown-dark w-205 pt-20 ml-20">
-                <img src="/search.svg" alt="Icon" height={35} width={35} />
+                <Image src="/search.svg" alt="Icon" height={35} width={35} />
                 <div className="font-mPlus text-brown-dark">エリアから探す</div>
               </div>
               <div className="lg:flex z-30">
@@ -17,7 +18,7 @@ export default function Search() {
                     小倉北区
                   </button>
                   <div className="flex justify-center w-full h-auto">
-                    <img src="/kokura.svg" alt="map" height={200} width={200} className="my-10 lg:mx-70"/>
+                    <Image src="/kokura.svg" alt="map" height={200} width={200} className="my-10 lg:mx-70"/>
                   </div>
                   <button type="button" className="flex rounded-full bg-brown font-mPlus font-semibold text-white text-sm justify-center items-center w-69 h-69 p-20 absolute bottom-10 right-10">
                     小倉南区
@@ -37,7 +38,7 @@ export default function Search() {
                   <li className="flex justify-center">
                     <button type="button" className="bg-brown-light border border-brown-dark rounded text-brown-dark font-mPlus font-semibold text-xl w-full lg:w-11/12 py-22 my-10 mb-20 mx-20 lg:mt-9">
                       <div>詳細検索</div>
-                    </button>          
+                    </button>
                   </li>
                 </ul>
               </div>
@@ -50,14 +51,14 @@ export default function Search() {
                   <div className="my-10">リノベーション事例</div>
                   <div className="text-xxs">これまでにリノベーションされた<br/>建物の一例を紹介！</div>
                   <div className="w-auto h-auto flex justify-end z-10">
-                    <img src="/house.svg" alt="Icon" className="w-35 h-35 lg:w-45 lg:h-45"/>
+                    <Image src="/house.svg" alt="Icon" className="w-35 h-35 lg:w-45 lg:h-45"/>
                   </div>
                 </div>
                 <div className="flex flex-col justify-center text-center bg-white rounded text-sm w-5/12 h-auto ml-10 mr-30">
                   <div className="my-10">経験者に相談</div>
                   <div className="text-xxs">リノベーション経験者の話を<br/>聞くならココ！</div>
                   <div className="w-auto h-auto flex justify-end z-10">
-                    <img src="/forum.svg" alt="Icon" className="w-35 h-35 lg:w-45 lg:h-45"/>
+                    <Image src="/forum.svg" alt="Icon" className="w-35 h-35 lg:w-45 lg:h-45"/>
                   </div>
                 </div>
               </div>
